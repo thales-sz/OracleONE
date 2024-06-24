986d374fcc76168081968b07d9485a5f9744ccb9
Thales Souza Chagas <thales.souz@outlook.com>
2024-06-24 13:53:40 -0300

    curso 02 - aula 04 - Listas

---
+++ Lógica de Programação: Funções - Curso 02/Aula 04 - Listas/desafio.js
@@ -0,0 +1,22 @@
+// Crie uma lista vazia, com o nome listaGenerica.
+const listaGenerica = [];
+
+// Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript', 'C', 'C++', 'Kotlin' e 'Python'.
+const linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+// Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+linguagensDeProgramacao.push('Java');
+linguagensDeProgramacao.push('Ruby');
+linguagensDeProgramacao.push('Golang');
+
+// Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+const firstNameList = ['Thales', 'Pyetra', 'Mael'];
+console.log(firstNameList[0]); // Thales
+
+// Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+const secondNameList = ['Thales', 'Pyetra', 'Mael'];
+console.log(secondNameList[1]); // Pyetra
+
+// Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+const thirdNameList = ['Thales', 'Pyetra', 'Mael'];
+console.log(thirdNameList[2]);  // Mael
